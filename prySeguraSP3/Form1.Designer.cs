f8f11e0b9c5e0c2a77c868e2e57141dbf9b6eb71
samiries <samiries21@gmail.com>
2026-09-25 17:56:54 -0300

    terminado

--- prySeguraSP3/Form1.Designer.cs
+++ prySeguraSP3/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace prySeguraSP3
 {
-    partial class Form1
+    partial class SistemaRepuestos
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SistemaRepuestos));
             cmbmarca = new ComboBox();
             cmborigen = new ComboBox();
             txtnumero = new TextBox();
@@ -37,13 +38,21 @@
             cmbmarcabuscar = new ComboBox();
             cmborigenbuscar = new ComboBox();
             btnbuscar = new Button();
+            lblmarcaguardar = new Label();
+            lblorigenguardar = new Label();
+            lblmarcabuscar = new Label();
+            lblorigenbuscar = new Label();
+            lblnum = new Label();
+            lbldesc = new Label();
+            lblprecio = new Label();
+            Lstresultado = new ListBox();
             SuspendLayout();
             // 
             // cmbmarca
             // 
             cmbmarca.DropDownStyle = ComboBoxStyle.DropDownList;
             cmbmarca.FormattingEnabled = true;
-            cmbmarca.Items.AddRange(new object[] { "P", "F", "R\t\t" });
+            cmbmarca.Items.AddRange(new object[] { "P", "F", "R" });
             cmbmarca.Location = new Point(73, 36);
             cmbmarca.Name = "cmbmarca";
             cmbmarca.Size = new Size(121, 23);
@@ -53,7 +62,7 @@
             // 
             cmborigen.DropDownStyle = ComboBoxStyle.DropDownList;
             cmborigen.FormattingEnabled = true;
-            cmborigen.Items.AddRange(new object[] { "N", "I\t" });
+            cmborigen.Items.AddRange(new object[] { "N", "I" });
             cmborigen.Location = new Point(73, 91);
             cmborigen.Name = "cmborigen";
             cmborigen.Size = new Size(121, 23);
@@ -70,17 +79,20 @@
             // 
             // txtdescripcion
             // 
-            txtdescripcion.Location = new Point(254, 65);
+            txtdescripcion.Location = new Point(254, 81);
             txtdescripcion.Name = "txtdescripcion";
             txtdescripcion.Size = new Size(100, 23);
             txtdescripcion.TabIndex = 3;
+            txtdescripcion.TextChanged += txtdescripcion_TextChanged;
+            txtdescripcion.KeyPress += txtdescripcion_KeyPress;
             // 
             // txtprecio
             // 
-            txtprecio.Location = new Point(254, 94);
+            txtprecio.Location = new Point(254, 133);
             txtprecio.Name = "txtprecio";
             txtprecio.Size = new Size(100, 23);
             txtprecio.TabIndex = 4;
+            txtprecio.TextChanged += txtprecio_TextChanged;
             txtprecio.KeyPress += txtprecio_KeyPress;
             // 
             // btnguardar
@@ -102,16 +114,18 @@
             cmbmarcabuscar.Name = "cmbmarcabuscar";
             cmbmarcabuscar.Size = new Size(121, 23);
             cmbmarcabuscar.TabIndex = 6;
+            cmbmarcabuscar.SelectedIndexChanged += cmbmarcabuscar_SelectedIndexChanged;
             // 
             // cmborigenbuscar
             // 
             cmborigenbuscar.DropDownStyle = ComboBoxStyle.DropDownList;
             cmborigenbuscar.FormattingEnabled = true;
             cmborigenbuscar.Items.AddRange(new object[] { "N", "I\t" });
-            cmborigenbuscar.Location = new Point(73, 251);
+            cmborigenbuscar.Location = new Point(73, 267);
             cmborigenbuscar.Name = "cmborigenbuscar";
             cmborigenbuscar.Size = new Size(121, 23);
             cmborigenbuscar.TabIndex = 7;
+            cmborigenbuscar.SelectedIndexChanged += cmborigenbuscar_SelectedIndexChanged;
             // 
             // btnbuscar
             // 
@@ -121,12 +135,94 @@
             btnbuscar.TabIndex = 8;
             btnbuscar.Text = "Buscar";
             btnbuscar.UseVisualStyleBackColor = true;
+            btnbuscar.Click += btnbuscar_Click;
             // 
-            // Form1
+            // lblmarcaguardar
+            // 
+            lblmarcaguardar.AutoSize = true;
+            lblmarcaguardar.Location = new Point(114, 9);
+            lblmarcaguardar.Name = "lblmarcaguardar";
+            lblmarcaguardar.Size = new Size(40, 15);
+            lblmarcaguardar.TabIndex = 9;
+            lblmarcaguardar.Text = "Marca";
+            // 
+            // lblorigenguardar
+            // 
+            lblorigenguardar.AutoSize = true;
+            lblorigenguardar.Location = new Point(111, 72);
+            lblorigenguardar.Name = "lblorigenguardar";
+            lblorigenguardar.Size = new Size(43, 15);
+            lblorigenguardar.TabIndex = 10;
+            lblorigenguardar.Text = "Origen";
+            // 
+            // lblmarcabuscar
+            // 
+            lblmarcabuscar.AutoSize = true;
+            lblmarcabuscar.Location = new Point(114, 180);
+            lblmarcabuscar.Name = "lblmarcabuscar";
+            lblmarcabuscar.Size = new Size(40, 15);
+            lblmarcabuscar.TabIndex = 11;
+            lblmarcabuscar.Text = "Marca";
+            // 
+            // lblorigenbuscar
+            // 
+            lblorigenbuscar.AutoSize = true;
+            lblorigenbuscar.Location = new Point(114, 238);
+            lblorigenbuscar.Name = "lblorigenbuscar";
+            lblorigenbuscar.Size = new Size(43, 15);
+            lblorigenbuscar.TabIndex = 12;
+            lblorigenbuscar.Text = "Origen";
+            // 
+            // lblnum
+            // 
+            lblnum.AutoSize = true;
+            lblnum.Location = new Point(280, 9);
+            lblnum.Name = "lblnum";
+            lblnum.Size = new Size(51, 15);
+            lblnum.TabIndex = 13;
+            lblnum.Text = "Numero";
+            // 
+            // lbldesc
+            // 
+            lbldesc.AutoSize = true;
+            lbldesc.Location = new Point(271, 62);
+            lbldesc.Name = "lbldesc";
+            lbldesc.Size = new Size(69, 15);
+            lbldesc.TabIndex = 14;
+            lbldesc.Text = "Descripción";
+            // 
+            // lblprecio
+            // 
+            lblprecio.AutoSize = true;
+            lblprecio.Location = new Point(280, 115);
+            lblprecio.Name = "lblprecio";
+            lblprecio.Size = new Size(40, 15);
+            lblprecio.TabIndex = 15;
+            lblprecio.Text = "Precio";
+            // 
+            // Lstresultado
+            // 
+            Lstresultado.FormattingEnabled = true;
+            Lstresultado.Location = new Point(73, 331);
+            Lstresultado.Name = "Lstresultado";
+            Lstresultado.Size = new Size(281, 124);
+            Lstresultado.TabIndex = 16;
+            // 
+            // SistemaRepuestos
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(561, 333);
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Center;
+            ClientSize = new Size(561, 496);
+            Controls.Add(Lstresultado);
+            Controls.Add(lblprecio);
+            Controls.Add(lbldesc);
+            Controls.Add(lblnum);
+            Controls.Add(lblorigenbuscar);
+            Controls.Add(lblmarcabuscar);
+            Controls.Add(lblorigenguardar);
+            Controls.Add(lblmarcaguardar);
             Controls.Add(btnbuscar);
             Controls.Add(cmborigenbuscar);
             Controls.Add(cmbmarcabuscar);
@@ -136,8 +232,9 @@
             Controls.Add(txtnumero);
             Controls.Add(cmborigen);
             Controls.Add(cmbmarca);
-            Name = "Form1";
-            Text = "Form1";
+            Name = "SistemaRepuestos";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Repuestos";
             Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
@@ -154,5 +251,13 @@
         private ComboBox cmbmarcabuscar;
         private ComboBox cmborigenbuscar;
         private Button btnbuscar;
+        private Label lblmarcaguardar;
+        private Label lblorigenguardar;
+        private Label lblmarcabuscar;
+        private Label lblorigenbuscar;
+        private Label lblnum;
+        private Label lbldesc;
+        private Label lblprecio;
+        private ListBox Lstresultado;
     }
 }
--- prySeguraSP3/Form1.cs
+++ prySeguraSP3/Form1.cs
@@ -1,27 +1,127 @@
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace prySeguraSP3
 {
-    public partial class Form1 : Form
+    public partial class SistemaRepuestos : Form
     {
-        public Form1()
+        public SistemaRepuestos()
         {
             InitializeComponent();
             btnguardar.Enabled = false;
-            cmborigen.SelectedIndexChanged += validarbtn;
-            cmbmarca.SelectedIndexChanged += validarbtn;
-            txtdescripcion.TextChanged += validarbtn;
-            txtnumero.TextChanged += validarbtn;
-            txtprecio.TextChanged += validarbtn;
+            btnbuscar.Enabled = false;
+            Lstresultado.Visible = false;
+            cmborigen.SelectedIndexChanged += validarbtnguardar;
+            cmbmarca.SelectedIndexChanged += validarbtnguardar;
+            txtdescripcion.TextChanged += validarbtnguardar;
+            txtnumero.TextChanged += txtnumero_TextChanged;
+            txtprecio.TextChanged += txtprecio_TextChanged;
         }
+
+        string[] reps = new string[100];
+        int[] numerosUsados = new int[100];
+        int repsidx = 0;
+
 
 
 
         private void btnguardar_Click(object sender, EventArgs e)
         {
+            if (repsidx >= 100)
+            {
+                MessageBox.Show("Ya cargaste 100 repuestos.", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                btnguardar.Enabled = false;
+                return;
+            }
 
+            string marca = cmbmarca.Text;
+            string origen = cmborigen.Text;
+            string num = txtnumero.Text;
+            string desc = txtdescripcion.Text;
+            string precio = txtprecio.Text;
+
+            int num2;
+            int.TryParse(num, out num2);
+
+
+            bool repetido = false;
+            for (int i = 0; i < repsidx; i++)
+            {
+                if (numerosUsados[i] == num2)
+                {
+                    repetido = true;
+                    break;
+                }
+            }
+
+            if (repetido)
+            {
+                MessageBox.Show("Ya existe un repuesto con ese número.", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string repuestolinea = marca + "|" + origen + "|" + num + "|" + desc + "|" + precio;
+
+            reps[repsidx] = repuestolinea;
+            numerosUsados[repsidx] = num2;
+            repsidx++;
+
+            if (repsidx == 100)
+            {
+                MessageBox.Show("Se completó la carga de los 100 repuestos. No se permiten más ingresos.",
+                    "Límite alcanzado", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                btnguardar.Enabled = false;
+            }
+
+            cmbmarca.SelectedIndex = -1;
+            cmborigen.SelectedIndex = -1;
+            txtnumero.Clear();
+            txtdescripcion.Clear();
+            txtprecio.Clear();
+            cmbmarca.Focus();
         }
 
 
-        private void validarbtn(object sender, EventArgs e)
+        private void btnbuscar_Click(object sender, EventArgs e)
+        {
+            Lstresultado.Visible = true;
+            Lstresultado.Items.Clear();
+
+            string marcabusqueda = cmbmarcabuscar.Text;
+            string origenbusqueda = cmborigenbuscar.Text;
+
+            int encontrados = 0;
+
+            for (int i = 0; i < repsidx; i++)
+            {
+                string[] partes = reps[i].Split('|');
+                string marca = partes[0];
+                string origen = partes[1];
+                string num = partes[2];
+                string desc = partes[3];
+                string precio = partes[4];
+
+                if (marca == marcabusqueda && origen == origenbusqueda)
+                {
+                    string lineaMostrar = num + " - " + desc + " - $" + precio;
+                    Lstresultado.Items.Add(lineaMostrar);
+                    encontrados++;
+                }
+            }
+
+            if (encontrados == 0)
+            {
+                Lstresultado.Items.Add("NO HUBO RESULTADOS EN LA BÚSQUEDA");
+            }
+
+            cmbmarcabuscar.Focus();
+        }
+
+
+
+        private void validarbtnguardar(object sender, EventArgs e)
         {
             if (cmbmarca.SelectedIndex > -1 &&
                 cmborigen.SelectedIndex > -1 &&
@@ -37,27 +137,44 @@ namespace prySeguraSP3
             }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
         private void txtprecio_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != ',')
             {
                 e.Handled = true;
             }
         }
 
-        private void txtnumero_TextChanged(object sender, EventArgs e)
+        private void txtprecio_TextChanged(object sender, EventArgs e)
         {
-            string textolimpio = "";
-            bool comausada = false;
+            string textoLimpio = "";
+            bool comaUsada = false;
+            txtprecio.MaxLength = 15;
 
-            
+            foreach (char c in txtprecio.Text)
+            {
+                if (char.IsDigit(c))
+                {
+                    textoLimpio += c;
+                }
+                else if (c == ',' && !comaUsada && textoLimpio.Length > 0)
+                {
+                    textoLimpio += c;
+                    comaUsada = true;
+                }
+            }
+
+            if (txtprecio.Text != textoLimpio)
+            {
+                int cursorPos = txtprecio.SelectionStart;
+                txtprecio.Text = textoLimpio;
+                txtprecio.SelectionStart = Math.Min(cursorPos, txtprecio.Text.Length);
+                return;
+            }
+
+            validarbtnguardar(sender, e);
+
         }
-
         private void txtnumero_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
@@ -65,5 +182,91 @@ namespace prySeguraSP3
                 e.Handled = true;
             }
         }
+        private void txtnumero_TextChanged(object sender, EventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            txt.MaxLength = 10;
+            string textoLimpio = "";
+
+            foreach (char c in txt.Text)
+            {
+                if (char.IsDigit(c))
+                {
+                    textoLimpio += c;
+                }
+            }
+
+            if (txt.Text != textoLimpio)
+            {
+                int cursorPos = txt.SelectionStart;
+                txt.Text = textoLimpio;
+                txt.SelectionStart = Math.Min(cursorPos, txt.Text.Length);
+                return;
+            }
+
+            validarbtnguardar(sender, e);
+        }
+
+        private void txtdescripcion_TextChanged(object sender, EventArgs e)
+        {
+            string textoLimpio = "";
+
+            foreach (char c in txtprecio.Text)
+            {
+                if (char.IsDigit(c))
+                {
+                    textoLimpio += c;
+                }
+                else if (c != '|')
+                {
+                    textoLimpio += c;
+
+                }
+            }
+            txtdescripcion.MaxLength = 25;
+        }
+
+        private void txtdescripcion_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '|')
+            {
+                e.Handled = true;
+            }
+        }
+
+
+        private void cmbmarcabuscar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbmarcabuscar.SelectedIndex != -1 && cmborigenbuscar.SelectedIndex != -1 && repsidx > 0)
+            {
+                btnbuscar.Enabled = true;
+            }
+            else
+            {
+                btnbuscar.Enabled = false;
+            }
+        }
+
+
+
+        private void cmborigenbuscar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbmarcabuscar.SelectedIndex != -1 && cmborigenbuscar.SelectedIndex != -1 && repsidx > 0)
+            {
+                btnbuscar.Enabled = true;
+            }
+            else
+            {
+                btnbuscar.Enabled = false;
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
     }
 }
